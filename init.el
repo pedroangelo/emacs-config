f2fe3cd7f47c70b25d2aca21e31956b3039d5164
Pedro Ângelo <pjfangelo@hotmail.com>
2023-09-09 15:10:34 +0100

    place commands to set custom options, such as modes and variables, in separate file

--- init.el
+++ init.el
@@ -6,9 +6,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+ '(ispell-dictionary nil)
  '(package-archives
-   '(("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")))
+	 '(("gnu" . "https://elpa.gnu.org/packages/")
+		 ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages '(haskell-mode solarized-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -20,40 +23,11 @@
 
 ;; CUSTOM SCRIPTS
 
+;; set custom options, such as setting variables and enabling / disabling modes
+(load "~/.emacs.d/set-custom-options")
+
 ;; automatic package installer
 (load "~/.emacs.d/auto-package-installer")
 
 ;; automatic theme changer
 ;; (load "~/.emacs.d/auto-theme-changer")
-
-;; CUSTOMIZATION OPTIONS
-
-; disable electric-indent-mode
-(electric-indent-mode -1)
-
-;; disable tool bar
-(tool-bar-mode -1)
-
-;; disable menu bar
-(menu-bar-mode -1)
-
-;; line numbers
-(global-display-line-numbers-mode t)
-
-;; Remember and restore the last cursor location of opened files
-(save-place-mode 1)
-
-;; display matching parenthesis
-(show-paren-mode 1)
-
-;; default tab width
-(setq-default tab-width 2)
-
-;; unused customization options
-
-;; display line numbers relative to current cursor line
-;; (setq display-line-numbers-type 'relative)
-
-;; display thin cursor
-;; (setq-default cursor-type 'bar)
-
---
+++ set-custom-options.el
@@ -0,0 +1,33 @@
+;; CONFIGURATION VARIABLES
+
+; disable electric-indent-mode
+(electric-indent-mode -1)
+
+;; disable tool bar
+(tool-bar-mode -1)
+
+;; disable menu bar
+(menu-bar-mode -1)
+
+;; line numbers
+(global-display-line-numbers-mode t)
+
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+
+;; display matching parenthesis
+(show-paren-mode 1)
+
+;; ENABLE / DISABLE MODES
+
+;; default tab width
+(setq-default tab-width 2)
+
+;; UNUSED
+
+;; display line numbers relative to current cursor line
+;; (setq display-line-numbers-type 'relative)
+
+;; display thin cursor
+;; (setq-default cursor-type 'bar)
+
