8c1b6fef9f86a6389de8c7e9c14e47dd4d4cc29f
Pedro Ângelo <pjfangelo@hotmail.com>
2023-09-21 11:36:43 +0100

    improve scripts/auto-theme-changer.el: add function to calculate time intervals

--- scripts/auto-theme-changer.el
+++ scripts/auto-theme-changer.el
@@ -7,7 +7,8 @@
 ; list of themes with corresponding starting times
 (setq list-theme-times
 			'(("05:00" . solarized-light)
-				("20:00" . solarized-dark)))
+				("20:00" . solarized-dark)
+				("22:00" . solarized-dark-high-contrast)))
 
 ;; (nth 1 chosen-themes)
 ;; (car (nth 1 chosen-themes))
@@ -16,6 +17,10 @@
 ;; list of starting times
 (setq themes-starting-time
 	(mapcar 'car list-theme-times))
+
+;; list of themes
+(setq list-themes
+	(mapcar 'cdr list-theme-times))
 
 (defun set-theme-timer (time)
 	"set a timer to call theme handler function"
@@ -35,17 +40,32 @@
 	(string-to-number 
 	 (substring (current-time-string) 14 16)))
 
-(defun get-new-theme ()
-	"check which theme to choose according to current time of day"
-	nil)
+;; (defun get-new-theme ()
+;; 	"check which theme to choose according to current time of day"
+;; 	(nil))
 
-(defun auto-theme-changer ()
-	"change theme automatically according to time of day"
-	(setq new-theme get-new-theme)
-	(if (equal new-theme current-theme)
-			nil
-		  (setq current-theme new-theme)
-			(load-theme current-theme t)))
+(defun get-time-intervals ()
+	"from list-theme-times list, build new list with pairs of time intervals"
+	(setq time-intervals 
+				(cl-mapcar #'cons themes-starting-time (cdr themes-starting-time)))
+	(setq final-time-interval
+				(cons 
+				 (nth (- (length themes-starting-time) 1) themes-starting-time)
+				 (nth 0 themes-starting-time)))
+	(append time-intervals (cons final-time-interval nil)))
 
-;; update theme on start
-(auto-theme-changer)
+(defun get-time-intervals-themes ()
+	"from list-theme-times list, build new list with pairs of time intervals and corresponding themes"
+	(cl-mapcar #'cons (get-time-intervals) list-themes))
+
+	
+;; (defun auto-theme-changer ()
+;; 	"change theme automatically according to time of day"
+;; 	(setq new-theme get-new-theme)
+;; 	(if (equal new-theme current-theme)
+;; 			nil
+;; 		  (setq current-theme new-theme)
+;; 			(load-theme current-theme t)))
+
+;; ;; update theme on start
+;; (auto-theme-changer)
