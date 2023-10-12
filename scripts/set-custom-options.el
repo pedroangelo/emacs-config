31e2ae3543a47d5e25e48102168b3d527c835ead
Pedro Ângelo <pjfangelo@hotmail.com>
2023-10-12 14:17:51 +0100

    improve scripts/set-custom-options.el: set to kill whole line, including newline instead of just clearing the line

--- scripts/set-custom-options.el
+++ scripts/set-custom-options.el
@@ -1,6 +1,6 @@
-;; CONFIGURATION VARIABLES
+;; ENABLE / DISABLE MODES
 
-; disable electric-indent-mode
+;; disable electric-indent-mode
 (electric-indent-mode -1)
 
 ;; disable tool bar
@@ -9,10 +9,10 @@
 ;; disable menu bar
 (menu-bar-mode -1)
 
-;; line numbers
+;; display line numbers
 (global-display-line-numbers-mode t)
 
-;; Remember and restore the last cursor location of opened files
+;; remember and restore the last cursor location of opened files
 (save-place-mode 1)
 
 ;; display matching parenthesis
@@ -21,27 +21,30 @@
 ;; update buffer when file changes on disk
 (global-auto-revert-mode 1)
 
-;; save previous emacs session
-;(desktop-save-mode 1)
-
-; set indentation to spaces instead of tabs
-(setq-default indent-tabs-mode nil)
-
-;; ENABLE / DISABLE MODES
+;; CONFIGURATION VARIABLES
 
 ;; default tab width
 (setq-default tab-width 2)
 
-;; wrap text at words (treats as not having a newline at the end)
+;; set indentation to spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; wrap text at words (doesn't treat as having a newline at the end)
 (setq-default word-wrap t)
+
+;; kills up to newline if not at column zero, kills the whole line (newline and all) if at column zero
+(setq kill-whole-line t)
 
 ;; UNUSED
 
-;; display line numbers relative to current cursor line
+;; ;; display line numbers relative to current cursor line
 ;; (setq display-line-numbers-type 'relative)
 
-;; display thin cursor
+;; ;; display thin cursor
 ;; (setq-default cursor-type 'bar)
 
-;; wrap text at words (treats as having a newline at the end)
+;; ;; wrap text at words (treats as having a newline at the end)
 ;; (global-visual-line-mode t)
+
+;; ;; save previous emacs session
+;; (desktop-save-mode 1)
