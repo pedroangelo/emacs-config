ab00c7216b3f0e0f136d89405a354477f78cd80a
Pedro Ângelo <pjfangelo@hotmail.com>
2023-10-29 12:57:29 +0000

    creat core/core-libraries.el: add several libraries (dash, seq and s)

--- core/core.el
+++ core/core.el
@@ -3,6 +3,7 @@
 
 (add-to-list 'load-path (expand-file-name "core/package-management" user-emacs-directory))
 (require 'core.package-management)
+(require 'core.libraries)
 
 (provide 'core) 
 ;;; core.el ends here
---
+++ core/core.libraries.el
@@ -0,0 +1,10 @@
+(use-package dash
+  :pin gnu)
+
+(use-package seq
+  :pin gnu)
+
+(use-package s)
+
+(provide 'core.libraries) 
+;;; core.libraries.el ends here
