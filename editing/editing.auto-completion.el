9c5a0bb5b2de7cf797b15fab22df56d7faf26c24
Pedro Ângelo <pjfangelo@hotmail.com>
2023-11-05 21:27:59 +0000

    improve editing/editing.auto-completion.el: delete company mode config preventing it from working properly

--- editing/editing.auto-completion.el
+++ editing/editing.auto-completion.el
@@ -1,9 +1,8 @@
 (use-package company
+  :pin gnu
   :config
-  (setq company-idle-delay nil)
-  (company-mode)
-  (add-hook 'after-init-hook 'global-company-mode)
-  :pin gnu)
+  ;; (setq company-idle-delay nil)
+  (add-hook 'after-init-hook 'global-company-mode))
 
 (provide 'editing.auto-completion) 
 ;;; editing.auto-completion.el ends here
