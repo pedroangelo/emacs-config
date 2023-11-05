04b1f960ea9d6a1b7ba8425cf81c137819fbd2a0
Pedro Ângelo <pjfangelo@hotmail.com>
2023-11-05 21:47:36 +0000

    improve editing/editing.indentation.el: add aggressive-indent-mode

--- editing/editing.indentation.el
+++ editing/editing.indentation.el
@@ -1,2 +1,7 @@
+(use-package aggressive-indent
+  :config
+  ;; (add-to-list 'aggressive-indent-excluded-modes 'emacs-lisp-mode)
+  (global-aggressive-indent-mode 1))
+
 (provide 'editing.indentation) 
 ;;; editing.indentation.el ends here
