f5f797f8642e12b345237d2697c8d63187314c22
Pedro Ângelo <pjfangelo@hotmail.com>
2023-11-09 21:59:08 +0000

    improve editing/editing.text-folding.el: add origami

--- editing/editing.el
+++ editing/editing.el
@@ -1,6 +1,7 @@
 (require 'editing.text-highlight)
 (require 'editing.indentation)
 (require 'editing.auto-completion)
-
+(require 'editing.text-folding)
+ 
 (provide 'editing) 
 ;;; editing.el ends here
---
+++ editing/editing.text-folding.el
@@ -0,0 +1,7 @@
+(use-package origami
+  :requires (dash s)
+  :config
+  (global-origami-mode))
+
+ (provide 'editing.text-folding) 
+;;; editing.text-folding.el ends here
