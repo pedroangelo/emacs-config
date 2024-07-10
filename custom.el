9bed2e3ab5fbe7049775f45ae8707a83c7989c34
Pedro Ângelo <pjfangelo@hotmail.com>
2024-07-11 00:29:11 +0100

    add and configure guess-language package

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard zoom use-package solarized-theme rainbow-mode origami markdown-mode highlight-indent-guides helpful haskell-mode dimmer company circadian auto-package-update all-the-icons aggressive-indent))
+   '(guess-language dashboard zoom use-package solarized-theme rainbow-mode origami markdown-mode highlight-indent-guides helpful haskell-mode dimmer company circadian auto-package-update all-the-icons aggressive-indent))
  '(zoom-size '(0.618 . 0.618)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
